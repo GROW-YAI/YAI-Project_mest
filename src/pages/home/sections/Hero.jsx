3db4be9faadd0402cb2241e23e2837fe205b5f2d
Nkunim Asaah Osei <oseinkunim@gmail.com>
2025-02-20 23:14:43 +0000

    Testing

--- src/pages/home/sections/Hero.jsx
+++ src/pages/home/sections/Hero.jsx
@@ -4,6 +4,7 @@ const Hero = () => {
   return (
     <div className="p-6 flex justify-center items-center align-middle h-[60vh] border-b-2">
       <h4 className="text-4xl font-bold">Hero Section</h4>
+      <h1>WELCOME</h1>
     </div>
   );
 };
