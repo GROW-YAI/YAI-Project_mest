b1264a8257b560815e45f77a37642b1536d8846b
dorcas <dorcas.nyamekye@medpharma.care>
2025-12-09 09:06:32 +0000

    who we are image change

--- src/pages/home/sections/AboutInnovator.jsx
+++ src/pages/home/sections/AboutInnovator.jsx
@@ -31,6 +31,8 @@ const pinValues = [
   }
 ];
 
+//might change circle to square
+
 const containerVariants = {
   hidden: { opacity: 0 },
   visible: { opacity: 1, transition: { staggerChildren: 0.2 } }
